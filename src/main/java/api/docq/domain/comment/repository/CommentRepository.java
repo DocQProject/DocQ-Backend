9f6bd468e103e780a9a5c4dd285edb2857f3784c
seungminJ <seungmin103@gmail.com>
2025-07-07 21:50:50 +0900

    fix(DQ-18): Add isDeleted check to CommentRepository query

--- src/main/java/api/docq/domain/comment/repository/CommentRepository.java
+++ src/main/java/api/docq/domain/comment/repository/CommentRepository.java
@@ -2,9 +2,11 @@ package api.docq.domain.comment.repository;
 
 import api.docq.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query("select c from Comment c where c.isDeleted = false ")
     List<Comment> findCommentsByPostId(Long postId);
 }
