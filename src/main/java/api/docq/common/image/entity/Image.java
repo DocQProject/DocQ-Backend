2c38313f1134d3b5bb145585b09a5f029bb73e0b
Seungmin-J <seungmin103@gmail.com>
2025-07-10 13:32:45 +0900

    refactor(DQ-19): Add unique constraint with imageURL in Image entity

--- src/main/java/api/docq/common/image/entity/Image.java
+++ src/main/java/api/docq/common/image/entity/Image.java
@@ -15,6 +15,7 @@ public class Image {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String imageUrl;
 
     private Long referenceId;
