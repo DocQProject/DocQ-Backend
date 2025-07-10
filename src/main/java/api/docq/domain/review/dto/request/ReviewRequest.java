db32bc5313bb26f6dc1f27217279723e34b912b4
Seungmin-J <seungmin103@gmail.com>
2025-07-10 09:29:21 +0900

    refactor(DQ-20):Add @Size for starPoint in ReviewRequest

--- src/main/java/api/docq/domain/review/dto/request/ReviewRequest.java
+++ src/main/java/api/docq/domain/review/dto/request/ReviewRequest.java
@@ -12,9 +12,10 @@ public class ReviewRequest {
     @NotBlank(message = "내용을 입력하세요")
     @Size(min = 5, max = 200)
     private final String content;
-
+    
+    @Size(min = 1, max = 5)
     private final Integer starPoint;
-
+    
     public ReviewRequest(String content, Integer starPoint) {
         this.content = content;
         this.starPoint = starPoint;
