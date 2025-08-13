13a669c0fb529655c4de096e2e9187c57ce8a4ce
jiwon <clvlajrrhtlvek@gmail.com>
2025-08-13 21:26:58 +0900

    refactor(DQ-53): Add custom exception && valid handler

---
+++ src/main/java/api/docq/common/exception/ErrorCode.java
@@ -0,0 +1,28 @@
+package api.docq.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    //cCONFLICT
+    ALREADY_EXISTS_USER(HttpStatus.CONFLICT, "이미 존재하는 회원입니다."),
+
+    //NOT_FOUND
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+
+    //BAD_REQUEST
+
+
+    //FORBIDDEN
+
+
+    //UNAUTHORIZED
+    PASSWORD_MISMATCH(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+
+    private final HttpStatus errorCode;
+    private final String errorMessage;
+
+}
